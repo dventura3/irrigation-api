1e7d9d80e2fd78033fe4a9a23c35292722fe227b
Ventura Daniela <daniela1988@gmail.com>
2015-03-30 10:12:33 +0200

    Initial http requests

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ var fs = require('fs');
 var express = require('express');
 var bodyParser = require('body-parser');
 
+var sensors = [];
+var actuators = [];
 
 /*--------------- Express configuration goes here: -------------------*/
 var app = express();
@@ -26,10 +28,38 @@ app.listen(SERVER_PORT);
 
 /*--------------- HTTP Calls -------------------*/
 
-
-var getSensorValue = function(){
+//all the sensors
+var getSensorsValues = function(req, res){
   console.log("Sensors");
+  res.send({success:true});
 }
 
+//just one sensor
+var getSensorValues = function(req, res){
+  var sensorID = req.params.sensorID;
+  console.log("Sensor ID: " + sensorID);
+  var newValue = Math.random();
 
-app.get("/sensors", getSensorValue);
+  res.send({sensorID : sensorID, value : newValue});
+}
+
+//all the actuators
+var getActuatorsValues = function(req, res){
+  console.log("Actuators");
+  res.send({success:true});
+}
+
+//just one actuator
+var getActuatorValues = function(req, res){
+  var actuatorID = req.params.actuatorID;
+  console.log("Actuator ID: " + actuatorID);
+  var newValue = Math.random();
+
+  res.send({actuatorID : actuatorID, value : newValue});
+}
+
+app.get("/sensors", getSensorsValues);
+app.get("/sensors/:sensorID", getSensorValues);
+
+app.get("/actuators", getActuatorsValues);
+app.get("/actuators/:actuatorID", getActuatorValues);
