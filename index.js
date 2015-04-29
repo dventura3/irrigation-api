adc93a29d3535e5c9d860db7be9372cb56af9b5b
Ventura Daniela <daniela1988@gmail.com>
2015-04-29 15:49:51 +0200

    Add HTTP options

--- index.js
+++ index.js
@@ -22,11 +22,7 @@ app.use(
     res.header('Access-Control-Allow-Methods','GET,PUT,POST,DELETE,OPTIONS');
     res.header('Access-Control-Allow-Headers','Content-Type, Authorization, Content-Length, X-Requested-With');
 
-    if('OPTIONS' == req.method){
-        res.send(200);
-    }
-    else
-        next();
+    next(); //pass the controll to the next handler
   }
 );
 app.use(bodyParser.json());
@@ -233,6 +229,58 @@ var deletePlant = function(req, res){
   res.send({success:false});
 }
 
+/**** OPTIONS ***/
+
+var getDescriptions = function(req, res){
+  var uriPattern = new RegExp(req.params[0].replace(/\//g, '-').replace(/\d+/g, 'id') + '.*');
+  
+  fs.readdir(__dirname + "/public/RESTdesc_descriptions/board" , function (err, fileNames) { 
+    if(err) throw err;
+    fileNames = fileNames.filter(function (file) { return file.match(uriPattern); })
+                         .sort(function (a,b) { return b.length - a.length; });
+
+    if(!fileNames.length)
+      return res.send(404);
+
+    readFiles(__dirname + "/public/RESTdesc_descriptions/board", fileNames, function (files) {
+      console.log("Send RESTdesc descriptions");
+      res.header('Content-Type', 'text/n3');
+      res.send(joinN3Documents(files));
+    });
+
+  });
+}
+
+/*--------------- Helpers Functions -------------------*/
+
+function readFiles(directory, fileNames, callback) {
+  var files = [];
+  fileNames.forEach(function (fileName) {
+    fs.readFile(directory + '/' + fileName, 'utf-8', function (err, data) {
+      files.push(data);
+      if(files.length == fileNames.length)
+        callback(files);
+    });
+  });
+}
+
+function joinN3Documents(documents) {
+  var namespaces = '', usedNamespaces = {}, triples = '',
+      match, prefixMatcher = /^@prefix.*\.$\n/gm;
+  documents.forEach(function (document) {
+    while((match = prefixMatcher.exec(document)) && (match = match[0]))
+      if(!usedNamespaces[match])
+        namespaces += (usedNamespaces[match] = match);
+    triples += document.replace(prefixMatcher, '');
+  });
+  return namespaces + triples;
+}
+
+
+/*--------------- Routes -------------------*/
+
+
+app.options(/^\/([\d\w\/]*)$/, getDescriptions);
 
 app.get("/", getEntryPoint);
 
